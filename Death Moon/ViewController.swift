db2b7a79ef50179e60f75e038f87d7c6f9ed3a7c
Matthew Garrod <mgarrod@gaig.com>
2021-01-04 15:55:39 -0500

    add cresent shadow based on location

--- Death Moon/ViewController.swift
+++ Death Moon/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var fraction = 1.0
     var waxing = true
     var heading = "---"
+    var angle = 100.0
     var doOffset = false
     
     private let graphicsOverlay = AGSGraphicsOverlay()
@@ -92,32 +93,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let today = Date()
-        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: today)!
-        let moon = suncalc.getMoonIllumination(date: today)
-        let moonYesterday = suncalc.getMoonIllumination(date: yesterday)
-//        print(moon["fraction"]!)
-//        print(CGFloat(moon["phase"]!) * 180 / CGFloat(Double.pi))
-        fraction = moon["fraction"]! // percent illuminated
-//        print(fraction)
-        let fractionYesterday = moonYesterday["fraction"]! // percent illuminated
-//        print(fractionYesterday)
-//        print(CGFloat(moon["angle"]!) * 180 / CGFloat(Double.pi))
-        
-        let newImage = drawImage(fraction: fraction, fractionYesterday: fractionYesterday)
-        
-        deathMoonSymbol = AGSPictureMarkerSymbol(image: newImage)
-        
-        deathMoonSymbol.height = 100
-        deathMoonSymbol.width = 100
-        
-        // test
-//        let point = AGSPoint(x: -84.47016200393804, y: 39.23550174436833, z: 100, spatialReference: .wgs84())
-//        let graphic = AGSGraphic(geometry: point, symbol: deathMoonSymbol, attributes: nil)
-//        self.graphicsOverlay.graphics.add(graphic)
-        
-        //arView.startTracking(.initial, completion: nil)
-        arView.startTracking(.continuous, completion: nil)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
@@ -153,6 +128,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
     }
     
+    private func setupImage() {
+        
+        let today = Date()
+        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: today)!
+        let moon = suncalc.getMoonIllumination(date: today)
+        let moonYesterday = suncalc.getMoonIllumination(date: yesterday)
+//        print(moon["fraction"]!)
+//        print(CGFloat(moon["phase"]!) * 180 / CGFloat(Double.pi))
+        fraction = moon["fraction"]! // percent illuminated
+//        print(fraction)
+        let fractionYesterday = moonYesterday["fraction"]! // percent illuminated
+//        print(fractionYesterday)
+//        print(CGFloat(moon["angle"]!) * 180 / CGFloat(Double.pi))
+        
+        let newImage = drawImage(fraction: fraction, fractionYesterday: fractionYesterday)
+        
+        deathMoonSymbol = AGSPictureMarkerSymbol(image: newImage)
+        
+        deathMoonSymbol.height = 100
+        deathMoonSymbol.width = 100
+        
+        // test
+//        let point = AGSPoint(x: -84.47016200393804, y: 39.23550174436833, z: 100, spatialReference: .wgs84())
+//        let graphic = AGSGraphic(geometry: point, symbol: deathMoonSymbol, attributes: nil)
+//        self.graphicsOverlay.graphics.add(graphic)
+        
+        //arView.startTracking(.initial, completion: nil)
+        arView.startTracking(.continuous, completion: nil)
+    }
+    
     private func drawImage(fraction: Double, fractionYesterday: Double) -> UIImage {
         
         // wanning 50% or more illumination (tie x to left and lessThan50Illumination = false)
@@ -174,10 +179,52 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         controlx = (!waxing && fraction < 0.5) || (waxing && fraction >= 0.5) ? 800 - controlx : controlx // control2x = 800 - (control1x)
         control1y = Int(-1*((reverseFraction*200)-((1-reverseFraction)*160)))
         
-        let to1 = CGPoint(x: tox, y: to1y)
-        let to2 = CGPoint(x: tox, y: 800 - (to1y))
-        let control1 = CGPoint(x: controlx, y: control1y)
-        let control2 = CGPoint(x: controlx, y: 800 - (control1y))
+        var to1x = tox
+        var to2x = tox
+        var control1x = controlx
+        var control2x = controlx
+        
+        var to2y = 800-to1y
+        var control2y = 800-control1y
+        
+        let xorigin = 400
+        let yorigin = -400
+        angle = angle * (Double.pi / 180)
+        let to1d = sqrt(pow(Double(xorigin-tox),2)+pow(Double(yorigin-(-1*to1y)),2))
+        let to2d = sqrt(pow(Double(xorigin-tox),2)+pow(Double(yorigin-(-1*to2y)),2))
+        let control1d = sqrt(pow(Double(xorigin-controlx),2)+pow(Double(yorigin-(-1*control1y)),2))
+        let control2d = sqrt(pow(Double(xorigin-controlx),2)+pow(Double(yorigin-(-1*control2y)),2))
+        
+        var to1angle = fmod(atan2(Double((-1*to1y)-yorigin), Double(to1x-xorigin)) + Double.pi * 2, Double.pi * 2)
+        to1angle = (to1angle * 180/Double.pi) - (angle * 180/Double.pi)
+        to1angle = to1angle < 0 ? 360 + to1angle : to1angle
+        to1angle = to1angle * Double.pi / 180
+        var to2angle = fmod(atan2(Double((-1*to2y)-yorigin), Double(to2x-xorigin)) + Double.pi * 2, Double.pi * 2)
+        to2angle = (to2angle * 180/Double.pi) - (angle * 180/Double.pi)
+        to2angle = to2angle < 0 ? 360 + to2angle : to2angle
+        to2angle = to2angle * Double.pi / 180
+        var control1angle = fmod(atan2(Double((-1*control1y)-yorigin), Double(control1x-xorigin)) + Double.pi * 2, Double.pi * 2)
+        control1angle = (control1angle * 180/Double.pi) - (angle * 180/Double.pi)
+        control1angle = control1angle < 0 ? 360 + control1angle : control1angle
+        control1angle = control1angle * Double.pi / 180
+        var control2angle = fmod(atan2(Double((-1*control2y)-yorigin), Double(control2x-xorigin)) + Double.pi * 2, Double.pi * 2)
+        control2angle = (control2angle * 180/Double.pi) - (angle * 180/Double.pi)
+        control2angle = control2angle < 0 ? 360 + control2angle : control2angle
+        control2angle = control2angle * Double.pi / 180
+        
+        to1x = xorigin + Int(to1d * cos(to1angle))
+        to1y = -1*(yorigin + Int(to1d * sin(to1angle)))
+        to2x = xorigin + Int(to2d * cos(to2angle))
+        to2y = -1*(yorigin + Int(to2d * sin(to2angle)))
+        control1x = xorigin + Int(control1d * cos(control1angle))
+        control1y = -1*(yorigin + Int(control1d * sin(control1angle)))
+        control2x = xorigin + Int(control2d * cos(control2angle))
+        control2y = -1*(yorigin + Int(control2d * sin(control2angle)))
+        
+        let to1 = CGPoint(x: to1x, y: to1y)
+        let to2 = CGPoint(x: to2x, y: to2y)
+        let control1 = CGPoint(x: control1x, y: control1y)
+        let control2 = CGPoint(x: control2x, y: control2y)
         
 //        print("to1: ", to1)
 //        print("to2: ", to2)
@@ -229,49 +276,56 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         //https://www.timeanddate.com/moon/@37.46423,-77.67785
         //https://ipgeolocation.io/astronomy-api.html
         //https://www.mooncalc.org/#/37.3686,-78.962,2/2020.12.21/15:53/1/3
-
-        let moonPos = suncalc.getMoonPosition(date: Date(), lat: locValue.latitude, lng: locValue.longitude)
-
-        var azimuth: Double? = moonPos["azimuth"] ?? 0
-        //print(azimuth! * 180 / Double.pi)
-        azimuth = 180 + (azimuth! * 180 / Double.pi)
-        let altitude: Double? = moonPos["altitude"] ?? 0
-
-        let distance = moonPos["distance"] ?? 400000
-        deathMoonSymbol.height = CGFloat((363104 / distance) * 100)
-        deathMoonSymbol.width = CGFloat((363104 / distance) * 100)
-        if (doOffset) {
-            deathMoonSymbol.offsetY = -1 * (CGFloat((363104 / distance) * 100) / 2)
+        
+        if (angle > 99.0) {
+            angle = locValue.latitude
+            setupImage()
         }
         else {
-            deathMoonSymbol.offsetY = 0
+
+            let moonPos = suncalc.getMoonPosition(date: Date(), lat: locValue.latitude, lng: locValue.longitude)
+
+            var azimuth: Double? = moonPos["azimuth"] ?? 0
+            //print(azimuth! * 180 / Double.pi)
+            azimuth = 180 + (azimuth! * 180 / Double.pi)
+            let altitude: Double? = moonPos["altitude"] ?? 0
+
+            let distance = moonPos["distance"] ?? 400000
+            deathMoonSymbol.height = CGFloat((363104 / distance) * 100)
+            deathMoonSymbol.width = CGFloat((363104 / distance) * 100)
+            if (doOffset) {
+                deathMoonSymbol.offsetY = -1 * (CGFloat((363104 / distance) * 100) / 2)
+            }
+            else {
+                deathMoonSymbol.offsetY = 0
+            }
+
+            // based on 1 kilometer away
+            let z = 1000 * tan(altitude!)
+
+            let point = AGSPoint.init(x: locValue.longitude, y: locValue.latitude, z: z, spatialReference: .wgs84())
+
+            let points = AGSGeometryEngine.geodeticMove([point], distance: 1, distanceUnit: .kilometers(), azimuth: azimuth!, azimuthUnit: .degrees(), curveType: .geodesic )
+
+            let graphic = AGSGraphic(geometry: points![0], symbol: deathMoonSymbol, attributes: nil)
+            if self.graphicsOverlay.graphics.count > 0 {
+                self.graphicsOverlay.graphics.removeObject(at: 0)
+            }
+            //if (altitude! > 0) {
+                self.graphicsOverlay.graphics.add(graphic)
+            //}
+    //
+    //        let graphic2 = AGSGraphic(geometry: points![0], symbol: AGSSimpleMarkerSymbol(style: .triangle, color: .blue, size: 14), attributes: nil)
+    //        self.graphicsOverlay.graphics.add(graphic2)
+
+            let waxingString = waxing ? "waxing" : "waning";
+            //thelabel.text = String(format: "azimuth: %.02f\naltitude: %.02f\nz: %.02f\nfraction: %d%%\nwaxing: \(waxingString)",azimuth!,altitude! * 180 / Double.pi,z,Int(fraction * 100))
+            thelabel.text = String(format: "my heading: \(heading)°\nazimuth: %.02f°\naltitude: %.02f°\nfraction: %.01f%%\nphase: \(waxingString)",azimuth!,altitude! * 180 / Double.pi,fraction * 100)
+            
+    //        print("azimuth: ", azimuth!)
+    //        print("altitude: ", altitude! * 180 / 3.14)
+    //        print("z: ", z2)
         }
-
-        // based on 1 kilometer away
-        let z = 1000 * tan(altitude!)
-
-        let point = AGSPoint.init(x: locValue.longitude, y: locValue.latitude, z: z, spatialReference: .wgs84())
-
-        let points = AGSGeometryEngine.geodeticMove([point], distance: 1, distanceUnit: .kilometers(), azimuth: azimuth!, azimuthUnit: .degrees(), curveType: .geodesic )
-
-        let graphic = AGSGraphic(geometry: points![0], symbol: deathMoonSymbol, attributes: nil)
-        if self.graphicsOverlay.graphics.count > 0 {
-            self.graphicsOverlay.graphics.removeObject(at: 0)
-        }
-        //if (altitude! > 0) {
-            self.graphicsOverlay.graphics.add(graphic)
-        //}
-//        
-//        let graphic2 = AGSGraphic(geometry: points![0], symbol: AGSSimpleMarkerSymbol(style: .triangle, color: .blue, size: 14), attributes: nil)
-//        self.graphicsOverlay.graphics.add(graphic2)
-
-        let waxingString = waxing ? "waxing" : "waning";
-        //thelabel.text = String(format: "azimuth: %.02f\naltitude: %.02f\nz: %.02f\nfraction: %d%%\nwaxing: \(waxingString)",azimuth!,altitude! * 180 / Double.pi,z,Int(fraction * 100))
-        thelabel.text = String(format: "my heading: \(heading)°\nazimuth: %.02f°\naltitude: %.02f°\nfraction: %.01f%%\nphase: \(waxingString)",azimuth!,altitude! * 180 / Double.pi,fraction * 100)
-        
-//        print("azimuth: ", azimuth!)
-//        print("altitude: ", altitude! * 180 / 3.14)
-//        print("z: ", z2)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
